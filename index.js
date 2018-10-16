f9710805b8bd1b08451c675cb8e0f1db9983bfc9
Gabriel Hernandez <webspinner.gabriel@gmail.com>
2018-10-16 13:39:37 -0700

    get server up and answering calls

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const https = require('https')
+const http = require('http')
+
+const PORT = 8080
+
+function handleRequest(req, res) {
+  res.end('Everything works!')
+}
+
+const server = http.createServer({}, handleRequest)
+server.listen(PORT, function(){
+  console.log('server listening on ' + PORT)
+})
