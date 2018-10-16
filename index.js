e5bb8924b77d20a46b59c5cd38a63b322eb7cfab
Gabriel Hernandez <webspinner.gabriel@gmail.com>
2018-10-16 16:55:28 -0700

    add wow scope

--- index.js
+++ index.js
@@ -8,17 +8,16 @@ const {
 const passport = require('passport')
 const express = require('express')
 const BnetStrategy = require('passport-bnet').Strategy
-const https = require('https')
-const http = require('http')
 
 passport.use(new BnetStrategy({
   clientID: BLIZZARD_API_KEY,
   clientSecret: BLIZZARD_SECRET,
   callbackURL: "https://www.wowwebtools.com/auth/bnet/callback",
-  region: "us"
+  region: "us",
+  scope: ['wow.profile']
 }, function(accessToken, refreshToken, profile, done) {
   console.log(accessToken, refreshToken, profile)
-  return done(null, profile);
+  done(null, profile)
 }));
 
 const PORT = parseInt(process.argv[2], 10)
@@ -33,7 +32,8 @@ app.get('/auth/bnet',
 app.get('/auth/bnet/callback',
     passport.authenticate('bnet', { failureRedirect: '/' }),
     function(req, res){
-        res.redirect('/');
-    });
+      console.log('we got through')
+      res.redirect('/');
+    })
 
 app.listen(PORT, () => { console.log(`server running on ${PORT}`) })
